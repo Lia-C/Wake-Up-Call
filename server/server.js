24243122481d62b9af39288ddc48a88dc4f51737
Win Wang <fallenpwr@gmail.com>
2014-10-05 09:14:36 -0400

    added a comma

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ if (Meteor.isServer) {
     Scripts.remove({});
 
     var testScript = {
-    	linesA: "I haven't got a brain... only straw.\nI don't know... But some people without brains do an awful lot of talking... don't they?"
+    	linesA: "I haven't got a brain... only straw.\nI don't know... But some people without brains do an awful lot of talking... don't they?",
       linesB: "How can you talk if you haven't got a brain?\nYes, I guess you're right."
     }
 
